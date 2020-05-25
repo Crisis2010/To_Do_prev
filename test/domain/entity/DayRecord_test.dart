cb6253620cd74a7e036a27d38b582a81c232a9ec
Philipp <Crisis4433@gmail.com>
2020-05-25 16:27:29 +0300

    Added DayMemo Test

---
+++ test/domain/entity/DayRecord_test.dart
@@ -0,0 +1,101 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:todo_app/domain/entity/DayMemo.dart';
+import 'package:todo_app/domain/entity/DayRecord.dart';
+import 'package:todo_app/domain/entity/ToDoRecord.dart';
+
+void main() {
+  DayRecord record;
+
+  const testDayMemo = DayMemo(year: 2020, month: 12, day: 11);
+
+  setUp(() {
+    record = DayRecord(dayMemo: testDayMemo, toDoRecords: [], isToday: false);
+  });
+
+  test(
+    'should return new Build by calling static method',
+    () async {
+      // arrange
+
+      // act
+      final result = record.buildNew();
+
+      // assert
+      expect(result.runtimeType, record.runtimeType);
+
+      // arrange
+      // when(mockCategoryRepository.getCategory(any)).thenAnswer((_) async => Right(tCategory));
+      // act
+      // final result = await usecase(Params(categoryId: tCategoryId));
+      // assert
+      // expect(result, Right(tCategory));
+      // verify(mockCategoryRepository.getCategory(tCategoryId));
+      // verifyNoMoreInteractions(mockCategoryRepository);
+    },
+  );
+
+  test(
+    'should change only toDoRecords with new Build by calling static method',
+    () async {
+      // arrange
+      final exprected = DayRecord(toDoRecords: [ToDoRecord()]);
+
+      // act
+      final result = record.buildNew(toDoRecords: [ToDoRecord()]);
+
+      // assert
+      expect(exprected.toDoRecords.length, result.toDoRecords.length);
+    },
+  );
+
+  test(
+    'should change only dayMemo with new Build by calling static method',
+    () async {
+      // arrange
+      final exprected = DayRecord(dayMemo: DayMemo());
+
+      // act
+      final result = record.buildNew(dayMemo: DayMemo());
+
+      // assert
+      expect(exprected.dayMemo.runtimeType, result.dayMemo.runtimeType);
+    },
+  );
+
+  test(
+    'should change only isToday with new Build by calling static method',
+    () async {
+      // arrange
+      final exprected = DayRecord(isToday: true);
+
+      // act
+      final result = record.buildNew(isToday: true);
+
+      // assert
+      expect(exprected.isToday, result.isToday);
+    },
+  );
+
+  test(
+    'should check key before update dayMemo',
+    () async {
+      // arrange
+      final result = record.buildNewDayMemoUpdated(testDayMemo);
+
+      // assert
+      expect(result.dayMemo.year, testDayMemo.year);
+    },
+  );
+
+  test(
+    'should update dayMemo',
+    () async {
+      const newDayMemo = DayMemo(year: 2020, month: 12, day: 11);
+      // arrange
+      final result = record.buildNewDayMemoUpdated(newDayMemo);
+
+      // assert
+      expect(result.dayMemo.year, newDayMemo.year);
+    },
+  );
+}
