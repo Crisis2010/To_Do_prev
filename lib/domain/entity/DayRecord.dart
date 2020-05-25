2f9a3ebc5aeea1244d1522c71b4a3bfe0d5b2046
Philipp <Crisis4433@gmail.com>
2020-05-25 16:29:21 +0300

    Updated dayMemo test

--- lib/domain/entity/DayRecord.dart
+++ lib/domain/entity/DayRecord.dart
@@ -1,4 +1,3 @@
-
 import 'package:todo_app/domain/entity/DayMemo.dart';
 import 'package:todo_app/domain/entity/ToDoRecord.dart';
 
@@ -13,11 +12,7 @@ class DayRecord {
     this.isToday = false,
   });
 
-  DayRecord buildNew({
-    List<ToDoRecord> toDoRecords,
-    DayMemo dayMemo,
-    bool isToday,
-  }) {
+  DayRecord buildNew({List<ToDoRecord> toDoRecords, DayMemo dayMemo, bool isToday}) {
     return DayRecord(
       toDoRecords: toDoRecords ?? this.toDoRecords,
       dayMemo: dayMemo ?? this.dayMemo,
